d8c2903d0bd4600528c0951c76e87b4122ed0563
DevArtist <kevinwasonga415@outlook.com>
2024-02-23 04:04:37 +0300

    test-composition

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        /*String inspection methods - checks*/
 
+        //Length returns the number of char in a string
+        System.out.println("Love".length());
+
+        //Checks where a char is located
+        System.out.println("Love".charAt(2));
+
+        //Checks for the index of the char
+        System.out.println("Love".indexOf('e'));
+
+        //Checks for strings only
+        System.out.println(" ".isBlank());
+
+        //Checks for both whitespaces and strings
+        System.out.println(" ".isEmpty());
+
+
+        //Methods to compare strings
+
+        //String manipulation methods
     }
 
 }
