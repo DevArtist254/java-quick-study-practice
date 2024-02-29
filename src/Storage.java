c5e7b66a17dc9a6a053ec90537a82844952d322b
DevArtist254 <kevinwasonga415@outlook.com>
2024-02-29 06:08:04 +0300

    Arr-store-2

--- src/Storage.java
+++ src/Storage.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Storage {
     public Storage() {
@@ -19,6 +20,43 @@ public class Storage {
     }
 
     public static void main(String[] args) {
+
+        Random randomNum = new Random();
+        int[] genRanInt = new int[100];
+
+        for (int i = 0;i < 100; i++ ){
+            genRanInt[i] = randomNum.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(genRanInt));
+        Arrays.sort(genRanInt);
+        int[] ints = reverseSort(genRanInt);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public static int[] reverseSort(int[] arr) {
+        int[] copyArr = Arrays.copyOf(arr, arr.length);
+        int temp;
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+
+            for(int i = 0;i < copyArr.length - 1; i++ ){
+                if(copyArr[i] < copyArr[i + 1]){
+                    temp = copyArr[i];
+                    copyArr[i] = copyArr[i + 1];
+                    copyArr[i + 1] = temp;
+
+                    flag = true;
+                }
+            }
+        }
+
+        return copyArr;
+    }
+
+    public void start() {
         int[] numGam = {1, 20, 33, 4, 55, 6, 7, 8, 9, 10};
 
         Arrays.sort(numGam);
